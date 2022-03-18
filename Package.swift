3e590cb105824aebb39b50458d8af5db6303a0f1
Jérôme Blondon <jbl@beamapp.co>
2022-03-18 17:29:09 +0100

    Use ZippyJSONCFamily fork with (hopefully) fix for misaligned address

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
             targets: ["ZippyJSON"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/michaeleisel/JJLISO8601DateFormatter", .upToNextMajor(from: "0.1.2")),
-        .package(url: "https://github.com/michaeleisel/ZippyJSONCFamily", .exact("1.2.4")),
+        .package(url: "https://github.com/jbl2024/JJLISO8601DateFormatter", .upToNextMajor(from: "0.1.2")),
+        .package(url: "https://github.com/jbl2024/ZippyJSONCFamily", .revision("160a9633e2fe6bfe26c4971876f0f29fec1e91c3")),
     ],
     targets: [
         .target(
